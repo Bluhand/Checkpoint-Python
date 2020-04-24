e26a2f00f7f3cb2091841279c350ab675d430f71
Bluhand <61250137+Bluhand@users.noreply.github.com>
2020-04-23 22:35:55 -0300

    Add files via upload

---
+++ Checkpoint.py
@@ -0,0 +1,12 @@
+vazamentos = ["a_vanessinha_1990@hotmail.com","a3sign@pandora.be","aaanika2@hotmail.com","aaron2003s@bol.com.braaron","21@hotmail.com","abidoral@hotmail.com","abk_333@hotmail.com","abner_bim@hotmail.com","abner_bim@hotmail.com","acacio_divix@hotmail.com","academia.boaforma@yahoo.com.brac","afrancar15@yahoo.com","bragatha_rosa8@yahoo.com.br","agatha_rosa8@yahoo.com.br",
+"agente_nasa234@yahoo.com.br","profalberico.filho@fiap.com.br","agente_cia768@yahoo.com.br","agloco.com.fake@gmail.com","agloco-sv@bol.com.br","agnaldojt@hotmail.com","agoraeuvodetona2@yahoo.com","agoraeuvodetona2@yahoo.com","agricesar@yahoo.com.br","agricesar@yahoo.com.br","agricesar@yahoo.com.br","aguia_fenix@hotmail.com","aguinaldo4789@hotmail.com","aguinaldo4789@hotmail.com","aguinaldo4789@hotmail.com"]
+
+consulta = input("Qual o seu e-mail: ")
+
+for email in vazamentos:
+    if consulta == email:
+        print("Seu e-mail foi vazado, cuidado")
+    else:
+        print("Seu e-mail não foi vazado") 
+        
+        
